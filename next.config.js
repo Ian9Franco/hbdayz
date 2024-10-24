64afc7b695ef28278bbbbe933e738b1e18222eb3
Notorious <100317984+Ian9Franco@users.noreply.github.com>
2024-10-24 15:50:07 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true,  // Activa App Router
+  },
+  i18n: {
+    locales: ['en', 'es'],  // Idiomas disponibles
+    defaultLocale: 'en',     // Idioma por defecto
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
