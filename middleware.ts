40097d8a62065c53043552f49577db5848607b84
Notorious <100317984+Ian9Franco@users.noreply.github.com>
2024-10-24 17:16:41 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,15 @@
 import createMiddleware from 'next-intl/middleware';
-import { NextRequest } from 'next/server';
 
+// Crea el middleware para manejar la internacionalización
 export default createMiddleware({
+  // Define los idiomas soportados
   locales: ['en', 'es'],
+  // Establece el idioma por defecto
   defaultLocale: 'es'
 });
 
+// Configura el matcher para aplicar el middleware solo a las rutas necesarias
 export const config = {
+  // Aplica el middleware a todas las rutas excepto las de API, archivos estáticos y Vercel
   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
