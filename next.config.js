7b723b992e7a1a5605cbf3b1791c18b47cce903f
Notorious <100317984+Ian9Franco@users.noreply.github.com>
2024-10-24 17:42:09 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,17 +2,11 @@ const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Habilita el modo estricto de React para mejorar la detección de errores
   reactStrictMode: true,
-  // Utiliza SWC para la minificación, lo que puede mejorar el rendimiento de la compilación
   swcMinify: true,
-  // Genera una build independiente que puede ser desplegada sin depender de un servidor Node.js
-  output: 'standalone',
   experimental: {
-    // Habilita el nuevo App Router de Next.js
-    appDir: true,
+    appDir: true,  // Mantiene App Router activado
   },
 };
 
-// Exporta la configuración envuelta con el plugin de next-intl
 module.exports = withNextIntl(nextConfig);
