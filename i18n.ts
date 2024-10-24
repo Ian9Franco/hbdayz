20670d7483543ddde0b5255d46b70f21fc96537f
Notorious <100317984+Ian9Franco@users.noreply.github.com>
2024-10-24 17:53:26 -0300

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -1,12 +1,5 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async (context) => {
-  const locale = context.locale || 'en';
-  try {
-    const messages = (await import(`./messages/${locale}.json`)).default;
-    return { messages };
-  } catch (error) {
-    console.error(`Error loading messages for locale: ${locale}`, error);
-    return { messages: {} };
-  }
-});
+export default getRequestConfig(async ({ locale }) => ({
+  messages: (await import(`./messages/${locale}.json`)).default
+}));
